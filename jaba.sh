38d94f471d72dadacad9e6959057af7c57ada32b
Glitchyi <advaithnarayanan8@gmail.com>
2022-10-31 23:43:31 +0530

    Shift to python

--- jaba.sh
+++ jaba.sh
@@ -1,16 +1,29 @@
-filename=$(basename -- "$1")
-extension="${filename##*.}"
-filename="${filename%.*}"
+file=$(basename -- "$1")
+extension="${file##*.}"
+filename="${file%.*}"
+count=0
+total=34
+pstr="[=============================]"
 c="c" 
 java="java"
+echo --------------------------------------
+printf "\033[96mCompiling $filename of type $extension \n\033[0m"
+
+
+while [ $count -lt $total ]; do
+  sleep 0.005 # this is work
+  count=$(( $count + 1 ))
+  pd=$(( $count * 73 / $total ))
+  printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
+done
+echo
+echo --------------------------------------
+
+
 if [ "$extension" = "$c" ];then
-    echo compiling $filename of type $extension
-    echo --------------------------------------
     gcc $1 && ./a.out && rm a.out
 elif [ "$extension" = "$java" ]; then
-    echo compiling $filename of type $extension
-    echo --------------------------------------
-    javac -d $filename $1 && cd $filename && java $filename && cd .. && sudo rm -rf $filename
+    javac -d $filename $1 && cd $filename && java $filename && cd .. && rm -rf $filename
 else
     echo "jabac <file-name>"
 fi
